5d838d81e14d366f0d705a6a61ad010e4c9ed219
prakit.w <prakit.w@arise.tech>
2024-03-10 15:47:35 +0700

    add simple counter, test

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,9 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./page";
 
 describe("Main Page", () => {
   it("should display Hello World in document", () => {
     render(<Home />);
     expect(screen.getByText("Hello World")).toBeInTheDocument();
+  });
+
+  it("should click Add button and count should increase", () => {
+    const { getByText, getByRole } = render(<Home />);
+    expect(getByText(/Count: 0/i)).toBeInTheDocument();
+
+    const button = getByRole("button", { name: "Add" });
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+    expect(getByText(/Count: 1/i)).toBeInTheDocument();
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [count, setCount] = useState<number>(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <div>Hello World</div>
+      <div>Count: {count}</div>
+      <div>
+        <button onClick={handleClick}>Add</button>
+      </div>
+    </div>
+  );
 }
