8266f155dcc886446882fb0099ae48cafe0297ea
prakit.w <prakit.w@arise.tech>
2024-06-12 14:42:33 +0700

    add api instance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+"use client";
+import api from "@/api/instance";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState<number>(0);
 
-  const handleClick = () => {
+  useEffect(() => {
+    const callApi = async () => {
+      const posts = await api.get("/posts");
+      console.log(posts);
+    };
+    callApi();
+  }, []);
+
+  const handleClick = async () => {
     setCount(count + 1);
   };
 
